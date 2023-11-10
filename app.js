807f270f0d745dba099b798ba250ad04e4a30b24
legkovoyg <147488090+legkovoyg@users.noreply.github.com>
2023-11-10 19:29:10 +0500

    На проверку
    Проблема с всплывающим окном, а также с отображением единиц измерения в precipitation, humidity и т.д.

--- app.js
+++ app.js
@@ -134,15 +134,62 @@ const locations = [
   console.log(locations);
 
 window.addEventListener('load', function() {
-    const [, currentLocation] = locations;
+    const [currentLocation] = locations;
     const { name, country, days } = currentLocation;
-
+  console.log(days);
     const cityElement = document.getElementById('city');
     const countryElement = document.getElementById('country');
+    const weekElement = document.getElementById('week');
+    const precipitationElement = document.getElementById('first-percent');
+    const humidityElement = document.getElementById('second-percent');
+    const windElement = document.getElementById('WindPercent')
     if (cityElement) {
         cityElement.innerText = name;
     }
-    if (countryElement) {
+    if (countryElement) { 
         countryElement.innerText = country;
     }
+    if(weekElement) {
+      let needday = Object.entries(days[0]);
+      console.log(needday[6]);
+      let needday1 = needday[6]
+      console.log(needday1[1])
+
+
+      weekElement.innerText = Object.values(days[2]);
+      console.log(weekElement);
+    };
+    if (precipitationElement) {
+      let needday = Object.entries(days[0]);
+      let needday1 = needday[4]
+      precipitationElement.innerText = needday1[1];
+    }
+    if (humidityElement){
+      let needday = Object.entries(days[0]);
+      let needday1 = needday[5]
+      humidityElement.innerText = needday1[1]
+    }
+    if (windElement) {
+      let needday = Object.entries(days[0]);
+      let needday1 = needday[6]
+      
+      windElement.innerText = needday1[1]
+    }
 });
+
+
+// document.querySelector('left-block__location-place').innerHTML = '<span id="city"></span>, <span id="country"></span>'
+
+var closebutton = document.getElementById('btn-close'),
+  first = document.getElementById('first');
+  
+
+  closebutton.addEventListener('click', function() {
+  first.classList.remove('opened');
+  
+  });
+
+var openbutton = document.getElementById('btn'),
+  second = document.getElementById('first')
+  second.classList.add('opened')
+
