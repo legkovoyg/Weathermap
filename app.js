e0d4fea20aeb029719c0b5844accf6b762452404
legkovoyg <legkovoy@gmail.com>
2023-11-16 18:55:32 +0500

    Переделанный

--- app.js
+++ app.js
@@ -131,181 +131,207 @@ const locations = [
     }
   ];
 
-//City Select
-const citySelectElement = this.document.getElementById('city-select');
 
-locations.forEach(location => {
- const option = document.createElement('option');
- option.innerText = `${location.name}, ${location.country}`;
- option.value = location.id;
- option.id = location.id
- citySelectElement.appendChild(option);
-});
-let newCityClone = {};
-citySelectElement.onchange = function (){
-  const newCity = locations.find(location => location.id == citySelectElement.value);
-  for (let key in newCity){
-    newCityClone[key]=newCity[key]
-  }
-  console.log(newCity);
-  console.log(citySelectElement.value)};
-  console.log(newCityClone)
+  const App = {
+    init() {
+      //Сбор инфы с JSON
+      const [currentLocation] = locations;
+      this.showLocation(currentLocation);
 
+      this.modal();
+      this.selectListener();
+    },
 
+    modal() {
+      //Модальное окно
+      const modalElement = document.getElementById('first'); 
+      if (modalElement) {
+        document.querySelectorAll('.js-open-modal').forEach(button => {
+          button.addEventListener('click', () => {
+            modalElement.classList.remove('closed');
+          })
+        });
 
-//Сбор инфы с JSON
-  window.addEventListener('load', function() {
-  const [currentLocation] = locations;
-  const { name, country, days } = currentLocation;
-  const [ firstDay ] = days;
-  console.log(locations);
-  
-  
-//Переменные левый блок, верх
-  const LeftWeekElement = document.getElementById('week');
-  const LeftDateElement = document.getElementById('dateEl')
-  const cityElement = document.getElementById('city');
-  const countryElement = document.getElementById('country');
-//Переменные левый блок, низ
-  const LeftWeatherElementMark = document.getElementById('left-block__location-mark')
-  const LeftTempElement = document.getElementById('Temperature')
-  const LeftWeatherElement = document.getElementById('WeatherEl')
-//Переменные правый блок, верх
-  const precipitationElement = document.getElementById('first-percent');
-  const humidityElement = document.getElementById('second-percent');
-  const windElement = document.getElementById('WindPercent');
+        document.querySelectorAll('.js-close-modal').forEach(button => {
+          button.addEventListener('click',()=>{
+            modalElement.classList.add('closed');
+          })
+        });
+      }
+    },
+
+    selectListener() {
+      //City Select
+      const citySelectElement = document.getElementById('city-select');
+
+      citySelectElement.onchange =  () => {
+        const newCity = locations.find(location => location.id == citySelectElement.value);
+        console.log(this);
+        this.showLocation(newCity);
+      };
+
+      this.createSelectOptions(citySelectElement);
+    },
+
+    createSelectOptions(citySelectElement) {
+      locations.forEach(location => {
+        const option = document.createElement('option');
+        option.innerText = `${location.name}, ${location.country}`;
+        option.value = location.id;
+        option.id = location.id
+        citySelectElement.appendChild(option);
+      });
+    },
     
-//Weatherblock
-   const daysContainer = this.document.querySelector('.right-block__weatherblock');
-   days.forEach((day, index) => {
-    const { temperature, date, weather } = day;
-     const weekdayClassname = index !== 0 ? 'weatherblock__unit__week' : 'weatherblock_1st-unit__week';
-     const tempClassname = index!==0 ?'weatherblock__unit__temperature':'weatherblock_1st-unit__temperature';
-     const markClassname = index!==0 ? 'weatherblock__unit__mark':'weatherblock__1st-unit__mark';
-     const weekDayEl = createDivElement(weekdayClassname, date.slice(0,3));
-     const tempDayEl = createDivElement(tempClassname, temperature +'°C');
-     const image = document.createElement('img');
-          image.src= WeatherToMark(weather);
-          image.width=50;
-     const markDayEl = createDivElement(markClassname);
-           markDayEl.append(image);
-     const dayElname = index!==0?'right-block__weatherblock__unit':'right-block__weatherblock__1st-unit';
-     const dayEl = createDivElement(dayElname)
-     dayEl.append(markDayEl);
-     dayEl.append(weekDayEl);
-     dayEl.append(tempDayEl);
-     daysContainer.append(dayEl);
-   });
+    showLocation(location) {
+      const { name, country, days } = location;
+      const [ firstDay ] = days;
+      console.log(locations);
+      
+      
+    //Переменные левый блок, верх
+      const LeftWeekElement = document.getElementById('week');
+      const LeftDateElement = document.getElementById('dateEl')
+      const cityElement = document.getElementById('city');
+      const countryElement = document.getElementById('country');
+    //Переменные левый блок, низ
+      const LeftWeatherElementMark = document.getElementById('left-block__location-mark')
+      const LeftTempElement = document.getElementById('Temperature')
+      const LeftWeatherElement = document.getElementById('WeatherEl')
+    //Переменные правый блок, верх
+      const precipitationElement = document.getElementById('first-percent');
+      const humidityElement = document.getElementById('second-percent');
+      const windElement = document.getElementById('WindPercent');
+        
+    //Weatherblock
+      const daysContainer = document.querySelector('.right-block__weatherblock');
+      daysContainer.innerHTML = '';
 
-
-   
-//Раскидывание данных по HTML
-   if (cityElement) {
-       cityElement.innerText = name;
-   }
-   if (countryElement) { 
-       countryElement.innerText = country;
-   }
-   if (LeftWeekElement){
-    LeftWeekElement.innerText = weekChanger(firstDay.date.slice(0,3))
-   }
-   if (precipitationElement) {
-     precipitationElement.innerText =  `${firstDay.precipitation} %`;
-   }
-   if (humidityElement){
-     humidityElement.innerText = firstDay.humidity + ' %'
-   }
-  if (windElement) {
-    windElement.innerText = firstDay.wind + ' km/h';
-  }
-   if (LeftTempElement){
-    LeftTempElement.innerText = firstDay.temperature + '°C'
-   }
-   if (LeftWeatherElement){
-    LeftWeatherElement.innerText=zaglav(firstDay.weather)
-   }
-   if (LeftDateElement){
-     LeftDateElement.innerText = firstDay.date.slice(4,15)
-   }
-   if (LeftWeatherElementMark){
-   }
-
-//Модальное окно
-  const modalElement = this.document.getElementById('first'); 
-  if (modalElement) {
-     document.querySelectorAll('.js-open-modal').forEach(button => {
-      button.addEventListener('click', () => {
-        modalElement.classList.remove('closed');
-       })
-     });
-  }
-  if (modalElement){
-     document.querySelectorAll('.js-close-modal').forEach(button => {
-      button.addEventListener('click',()=>{
-        modalElement.classList.add('closed');
-      })
-     })
-   }});
-
-//Делает первую букву заглавной
-  function zaglav(str) {
-    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-    return(capitalize(str)) ;
-  }
-
-//Возвращает полное название недели 
-  function weekChanger(str) {
-    if (str=='Mon'){
-      str=Monday
-    }
-    else if  (str=="Thu"){
-      str="Thursday"
-    }
-    else if (str=='Wed'){
-      str='Wednesday'
-    }
-    else if (str=='Thu'){
-      str='Thursday'
-    }
-    else if (str=='Fry'){
-      str='Friday'
-    }
-    else if(str=='Sat'){
-      str='Saturday'
-    }
-    else if(str=='Sun'){
-      str="Sunday"
-    }
-    return str
-  }
-
-//Создание div элемента
-  function createDivElement(className, innerText) {
-    const el = document.createElement('div');
-    el.classList.add(className);
-
-    if (innerText) {
-      el.innerText = innerText
-    }
-
-    return el;
-  }
-
-//Отдает на погоду соответствующую иконку погоды
-  function WeatherToMark (weather){
-    if (weather == 'sunny')
-    {str ='./image/sunmark.svg'
+      console.log(daysContainer);
+      days.forEach((day, index) => {
+        const { temperature, date, weather } = day;
+        const weekdayClassname = index !== 0 ? 'weatherblock__unit__week' : 'weatherblock_1st-unit__week';
+        const tempClassname = index!==0 ?'weatherblock__unit__temperature':'weatherblock_1st-unit__temperature';
+        const markClassname = index!==0 ? 'weatherblock__unit__mark':'weatherblock__1st-unit__mark';
+        const weekDayEl = createDivElement(weekdayClassname, date.slice(0,3));
+        const tempDayEl = createDivElement(tempClassname, temperature +'°C');
+        const image = document.createElement('img');
+              image.src= WeatherToMark(weather);
+              image.width=50;
+        const markDayEl = createDivElement(markClassname);
+              markDayEl.append(image);
+        const dayElname = index!==0?'right-block__weatherblock__unit':'right-block__weatherblock__1st-unit';
+        const dayEl = createDivElement(dayElname)
+        dayEl.append(markDayEl);
+        dayEl.append(weekDayEl);
+        dayEl.append(tempDayEl);
+        daysContainer.append(dayEl);
+      });
     
-    }
-    else if (weather == 'rainy')
-    {str ='./image/rainymark.svg'
     
+      
+    //Раскидывание данных по HTML
+      if (cityElement) {
+          cityElement.innerText = name;
+      }
+      if (countryElement) { 
+          countryElement.innerText = country;
+      }
+      if (LeftWeekElement){
+        LeftWeekElement.innerText = weekChanger(firstDay.date.slice(0,3))
+      }
+      if (precipitationElement) {
+        precipitationElement.innerText =  `${firstDay.precipitation} %`;
+      }
+      if (humidityElement){
+        humidityElement.innerText = firstDay.humidity + ' %'
+      }
+      if (windElement) {
+        windElement.innerText = firstDay.wind + ' km/h';
+      }
+      if (LeftTempElement){
+        LeftTempElement.innerText = firstDay.temperature + '°C'
+      }
+      if (LeftWeatherElement){
+        LeftWeatherElement.innerText=zaglav(firstDay.weather)
+      }
+      if (LeftDateElement){
+        LeftDateElement.innerText = firstDay.date.slice(4,15)
+      }
+      if (LeftWeatherElementMark){
+      } 
     }
-    else if (weather == 'cloudy')
-    {str ='./image/cloudlymark.svg'
-    
-    }
-    return str
   };
 
+
+
+
+
+
+
+
+window.addEventListener('load', function() {
+ App.init();
+});
+
+
+//Делает первую букву заглавной
+function zaglav(str) {
+  const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+  return(capitalize(str)) ;
+}
+
+//Возвращает полное название недели 
+function weekChanger(str) {
+  if (str=='Mon'){
+    str=Monday
+  }
+  else if  (str=="Thu"){
+    str="Thursday"
+  }
+  else if (str=='Wed'){
+    str='Wednesday'
+  }
+  else if (str=='Thu'){
+    str='Thursday'
+  }
+  else if (str=='Fry'){
+    str='Friday'
+  }
+  else if(str=='Sat'){
+    str='Saturday'
+  }
+  else if(str=='Sun'){
+    str="Sunday"
+  }
+  return str
+}
+
+//Создание div элемента
+function createDivElement(className, innerText) {
+  const el = document.createElement('div');
+  el.classList.add(className);
+
+  if (innerText) {
+    el.innerText = innerText
+  }
+
+  return el;
+}
+
+//Отдает на погоду соответствующую иконку погоды
+function WeatherToMark (weather){
+  if (weather == 'sunny')
+  {str ='./image/sunmark.svg'
   
+  }
+  else if (weather == 'rainy')
+  {str ='./image/rainymark.svg'
+  
+  }
+  else if (weather == 'cloudy')
+  {str ='./image/cloudlymark.svg'
+  
+  }
+  return str
+};
